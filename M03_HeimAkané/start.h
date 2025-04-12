53294b688ca50b39a216a3970423689d9cc3be41
akanezilla <sheim6@gatech.edu>
2025-04-12 12:03:15 -0400

    m03 upload with no assembly

---
+++ M03_HeimAkané/start.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(start)
+
+//======================================================================
+//
+//	start, 240x160@4, 
+//	+ palette 256 entries, not compressed
+//	+ 600 tiles not compressed
+//	Total size: 512 + 19200 = 19712
+//
+//	Time-stamp: 2025-03-26, 00:24:09
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_START_H
+#define GRIT_START_H
+
+#define startTilesLen 19200
+extern const unsigned short startTiles[9600];
+
+#define startPalLen 512
+extern const unsigned short startPal[256];
+
+#endif // GRIT_START_H
+
+//}}BLOCK(start)
